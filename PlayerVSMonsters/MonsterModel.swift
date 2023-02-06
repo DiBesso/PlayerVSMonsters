285a9cb70081074e9b5b75076b6f57fbc804ba99
Dmitry <radiator211@mail.ru>
2023-02-06 21:44:36 +0700

    Create models

---
+++ PlayerVSMonsters/MonsterModel.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterModel.swift
+//  PlayerVSMonsters
+//
+//  Created by Дмитрий Бессонов on 06.02.2023.
+//
+
+import Foundation
+
+final class MonsterModel {
+    
+    private let attack = 15
+    private let shield = 20
+    private var health = [0...25]
+    private var damage = [1...6]
+    
+}
---
+++ PlayerVSMonsters/PlayerModel.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerModel.swift
+//  PlayerVSMonsters
+//
+//  Created by Дмитрий Бессонов on 06.02.2023.
+//
+
+import Foundation
+
+final class PlayerModel {
+    
+    private let attack = 10
+    private let shield = 20
+    private var health = [0...30]
+    private var damage = [1...6]
+    
+}
